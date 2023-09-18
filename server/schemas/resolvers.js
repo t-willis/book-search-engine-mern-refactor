2180d3476e4f27ce834590b3d7616dcc6d1a38d9
skybreakbb <willistaylora@gmail.com>
2023-09-17 19:18:07 -0500

    wrote more of resolvers.js and typeDefs.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,11 @@ const { User, Book } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
-
+    Query: {
+        me: async () => {
+            return User.find();
+        },
+    }
 }
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,31 @@
 const gql = require('graphql-tag');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Boolean
+        savedBooks: [Book]
+    },
 
+    type Book {
+        bookId: String
+        Authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    },
+
+    type Query {
+        me: User
+    },
 `;
 
 module.exports = typeDefs;
