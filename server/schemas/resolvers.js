bca6f1df0a3342e4f7ee30f878c31538e52d19b9
skybreakbb <willistaylora@gmail.com>
2023-09-17 19:39:08 -0500

    added mutation type to typeDefs, wrote addUser and login mutations in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,6 +6,31 @@ const resolvers = {
         me: async (parent, { username }) => {
             return User.findOne({ username });
         },
+    },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email, password });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        }
     }
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,6 +26,11 @@ const typeDefs = gql`
     type Query {
         me(username: String!): User
     },
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+    }
 `;
 
 module.exports = typeDefs;
