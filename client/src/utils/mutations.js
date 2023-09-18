141056f2a6578622a4a2e34a00457c4030365e40
skybreakbb <willistaylora@gmail.com>
2023-09-18 15:58:17 -0500

    added mutations.js and queries.js in client/src/utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,7 @@
+// mutations.js:
+// LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+// ADD_USER will execute the addUser mutation.
+// SAVE_BOOK will execute the saveBook mutation.
+// REMOVE_BOOK will execute the removeBook mutation.
+// Additionally, you’ll need to complete the following tasks in each of these front-end files:
+// App.jsx: Create an Apollo Provider to make every request work with the Apollo Server.
