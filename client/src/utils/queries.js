da315a231ded9d3e46b52ec8a32648bab14ea00d
skybreakbb <willistaylora@gmail.com>
2023-09-18 16:03:39 -0500

    wrote GET_ME query in queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,19 @@
-// queries.js: This will hold the query GET_ME, which will execute the me query set up using Apollo Server.
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query Query($username: String!) {
+    me(username: $username) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        title
+        bookId
+      }
+    }
+  }
+`;
+
+// queries.js: This will hold the query GET_ME,
+// which will execute the me query set up using Apollo Server.
